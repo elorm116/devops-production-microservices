df724e87508a5f22fb132248ed0e5d8832c57760
Anthony Elorm Zottor <aezottor@gmail.com>
2026-01-16 05:05:01 +0300

    updated the helm provider block to match helm provider version 2.9.0

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,7 +12,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 3.0.0"
+      version = ">= 2.9.0"
     }
   }
 }
@@ -37,11 +37,18 @@ provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    
+
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name", module.eks.cluster_name,
+        "--region", "us-east-1"  # optional but recommended
+      ]
     }
   }
 }
+
+
