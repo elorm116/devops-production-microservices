7bc9907e907c901279c64a6b56e515927e42a24f
Anthony Elorm Zottor <aezottor@gmail.com>
2026-01-16 04:07:32 +0300

    Added certificate arn variable

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -2,3 +2,7 @@ variable "vpc_id" {}
 variable "private_subnets" {
   type = list(string)
 }
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate for HTTPS"
+  type        = string
+}
